fab333b845402819f71dd763af4ded7dfdf0c32a
Lorenzo <l.scorzelli1@studenti.unisa.it>
2023-01-23 10:16:40 +0100

    Implementata la ricerca dei percorsi e aggiornamento meteo

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:visual_assistant/cronologiaPercorsi.dart';
 import 'package:visual_assistant/gestionePreferiti.dart';
 import 'package:visual_assistant/ricercaDestinazione.dart';
+
+//Bisogna runnare con: flutter run --no-sound-null-safety
 void main() {
   runApp(const MyApp());
 }
@@ -14,9 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Visual Assistant',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xFF0d7c97)
-      ),
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: const Color(0xFF0d7c97)),
       home: const MyHomePage(title: 'Visual Assistant'),
     );
   }
@@ -32,105 +33,100 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
       body: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(2),
-        child:
-          Column (
-            children: [
-              Row(
-                children: [
-                  SizedBox(width: 3),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>gestionePreferiti()));
-                    },
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Ink.image(
-                          image: AssetImage('assets/stella.png'),
-                          height: 320,
-                          width: 170,
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(2),
+          child: Column(children: [
+            Row(
+              children: [
+                SizedBox(width: 3),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => gestionePreferiti()));
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Ink.image(
+                        image: AssetImage('assets/stella.png'),
+                        height: 320,
+                        width: 170,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 8),
-                  InkWell(
-                    onTap: () {},
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Ink.image(
-                          image: AssetImage('assets/fotocamera.png'),
-                          height: 320,
-                          width: 170,
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
+                ),
+                SizedBox(width: 8),
+                InkWell(
+                  onTap: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Ink.image(
+                        image: AssetImage('assets/fotocamera.png'),
+                        height: 320,
+                        width: 170,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
                   ),
-
-                ],
-              ),
-              Row(
-                  children: [
-                    SizedBox(height: 10),
-                  ],
-              ),
-              //SizedBox(height: 20),
-              Row(
-                children: [
-                  SizedBox(width: 3),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>cronologiaPercorsi()));
-                    },
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Ink.image(
-                          image: AssetImage('assets/cronologia.png'),
-                          height: 320,
-                          width: 170,
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(height: 10),
+              ],
+            ),
+            //SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(width: 3),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => cronologiaPercorsi()));
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Ink.image(
+                        image: AssetImage('assets/cronologia.png'),
+                        height: 320,
+                        width: 170,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 8),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ricercaDestinazione()));
-                    },
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Ink.image(
-                          image: AssetImage('assets/lenteDiIngrandimento.png'),
-                          height: 320,
-                          width: 170,
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
+                ),
+                SizedBox(width: 8),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ricercaDestinazione()));
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Ink.image(
+                        image: AssetImage('assets/lenteDiIngrandimento.png'),
+                        height: 320,
+                        width: 170,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ])
-        ),
-      );
-
+                ),
+              ],
+            ),
+          ])),
+    );
   }
 }
--- lib/ricercaDestinazione.dart
+++ lib/ricercaDestinazione.dart
@@ -1,7 +1,15 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:visual_assistant/gestionePreferiti.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:geocoder/services/base.dart';
+import 'dart:async';
+
+import 'package:visual_assistant/viaggio.dart';
 
 class ricercaDestinazione extends StatefulWidget {
   const ricercaDestinazione({Key? key}) : super(key: key);
@@ -14,44 +22,65 @@ class _ricercaDestinazioneState extends State<ricercaDestinazione> {
   late stt.SpeechToText _speech;
   bool _isListening = false;
   String _textSpeech = "in attesa dell'indirizzo";
+  var locationMessage = "";
+  double? latitudineOri, longitudineOri; //coordinate origine
+  double? latitudineDest, longitudineDest; //coordinate destinazione
 
-  void onListen() async{
+  void getCurrentLocation() async {
+    var lastPosition = await Geolocator().getLastKnownPosition();
+    latitudineOri = lastPosition.latitude;
+    longitudineOri = lastPosition.longitude;
+  }
+
+  void convertiDestinazioneinCoordinate() async {
+    final query = _textSpeech;
+    var addresses = await Geocoder.local.findAddressesFromQuery(query);
+    var first = addresses.first;
+    latitudineDest = first.coordinates.latitude;
+    longitudineDest = first.coordinates.longitude;
+  }
+
+  void onListen() async {
     if (!_isListening) {
       bool available = await _speech.initialize(
         onStatus: (val) => print('onStatus: $val'),
         onError: (val) => print('onError: $val'),
       );
-      if (available){
+      if (available) {
         setState(() {
           _isListening = true;
         });
         _speech.listen(
-          onResult: (val) => setState((){
-            _textSpeech = val.recognizedWords;
-        })
-        );
+            onResult: (val) => setState(() {
+                  _textSpeech = val.recognizedWords;
+                }));
       }
-    }
-    else{
+    } else {
       setState(() {
         _isListening = false;
         _speech.stop();
-        showDialog(barrierDismissible: false, context: context, builder: (BuildContext context) => _buildPopupDialog(context),);
+        showDialog(
+          barrierDismissible: false,
+          context: context,
+          builder: (BuildContext context) => _buildPopupDialog(context),
+        );
       });
     }
   }
 
-  void initState(){
+  void initState() {
     super.initState();
     _speech = stt.SpeechToText();
   }
 
-  Widget _buildPopupDialog(BuildContext context)  {
+  Widget _buildPopupDialog(BuildContext context) {
     FlutterTts flutterTts = FlutterTts();
     flutterTts.setLanguage("it-IT");
     var a = flutterTts.getVoices;
-    flutterTts.setVoice({"name":"it-it-x-itd-local","locale":"it-IT"});
+    flutterTts.setVoice({"name": "it-it-x-itd-local", "locale": "it-IT"});
     flutterTts.speak("Indica se l'indirizzo " + _textSpeech + " è corretto?");
+    getCurrentLocation(); //trova la posizione dell'utente
+    convertiDestinazioneinCoordinate(); //converte la posizione (Stringa) in coordinate geografiche
 
     return AlertDialog(
       title: const Text(''),
@@ -65,26 +94,36 @@ class _ricercaDestinazioneState extends State<ricercaDestinazione> {
       actions: <Widget>[
         new ElevatedButton(
           onPressed: () {
-            Navigator.of(context).pop();
+            print("Latitudine origine: $latitudineOri" +
+                " | Longitudine origine: $longitudineOri");
+            print("Latitudine destinazione: $latitudineDest" +
+                " | Longitudine destinazione: $longitudineDest");
+
+          //  print("aspe' destinazione");
+
+            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>viaggio(latitudineOri:latitudineOri,longitudineOri: longitudineOri,latitudineDest: latitudineDest,longitudineDest: longitudineDest,)));
           },
-          style: ElevatedButton.styleFrom(minimumSize: Size(100, 100),),
+          style: ElevatedButton.styleFrom(
+            minimumSize: Size(100, 100),
+          ),
           child: const Text('Si'),
         ),
-        SizedBox(width: 40,),
+        SizedBox(
+          width: 40,
+        ),
         new ElevatedButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
-          style: ElevatedButton.styleFrom(minimumSize: Size(100, 100),),
+          style: ElevatedButton.styleFrom(
+            minimumSize: Size(100, 100),
+          ),
           child: const Text('No'),
         ),
       ],
     );
   }
-
-
-
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -92,31 +131,40 @@ class _ricercaDestinazioneState extends State<ricercaDestinazione> {
           title: const Text("Visual Assistant"),
         ),
         backgroundColor: Colors.grey[50],
-        body:SingleChildScrollView(
+        body: SingleChildScrollView(
             child: Container(
                 alignment: Alignment.center,
                 padding: const EdgeInsets.all(30),
-                child: Column (
-                    children: [
-                      Text("RICERCA PERCORSO", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-                      SizedBox(height: 60),
-                      Text(_textSpeech, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(40.0),),
-                            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 18),
-                            minimumSize: Size(300, 400),
-                            //side: BorderSide(width: 4.0, color: Color(0xff68240b)),
-                            primary: Color.fromRGBO(176, 224, 230, 1),
-                          ),
-                          onPressed: () => onListen(),
-                          child: Text('IMPOSTA DESTINAZIONE',style: TextStyle(color: Colors.black,fontSize: 20,),)
+                child: Column(children: [
+                  Text(
+                    "RICERCA PERCORSO",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 60),
+                  Text(
+                    _textSpeech,
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(40.0),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+                        minimumSize: Size(300, 400),
+                        //side: BorderSide(width: 4.0, color: Color(0xff68240b)),
+                        primary: Color.fromRGBO(176, 224, 230, 1),
                       ),
-                    ]
-                )
-            )
-        )
-    );
+                      onPressed: () => onListen(),
+                      child: Text(
+                        'IMPOSTA DESTINAZIONE',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      )),
+                ]))));
   }
 }
